69d3e3aae4c22b0a05136cfc4821a62a0622244b
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2025-08-27 05:24:46 -0400

    feat: replace TodoList with FolderTodos component in FolderPage and implement FolderTodos for displaying todos

--- src/components/FolderPage.tsx
+++ src/components/FolderPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom'
 import { useTodoContext } from '../context/TodoContext'
-import TodoList from './TodoList'
+import FolderTodos from './FolderTodos'
 
 export default function FolderPage () {
   const { sidebarState } = useTodoContext()
@@ -12,24 +12,22 @@ export default function FolderPage () {
   const folder = sidebarState.data.filter( (obj) => (obj.id === folderNumber) && (obj.type === 'folder'))
   const lists = sidebarState.data.filter( (obj) => {
     if (obj.folderId === folderNumber && obj.type === 'list') {
-      console.log('OBJ: ' + obj.name);
       return obj;
     }
   })
   const output = lists.map( (obj) => {
-    console.log(`${Date.now()} - ${obj.id}`);
+    console.log(obj.todos);
     return (
       <div key={`${Date.now()} - ${obj.id}`}>
-        <TodoList />
+        
+        <FolderTodos data={obj}/>
       </div>
     );
   })
-  console.log(lists)
   
 
   return (
     <>
-      <p>Folder page here!!!!</p>
       {output}
 
     </>
---
+++ src/components/FolderTodos.tsx
@@ -0,0 +1,45 @@
+import type { Todo } from '../types/todo'
+
+
+type Props = {
+  data: {
+    type: 'list',
+    id: number,
+    name: string,
+    todos: Todo[],
+    folderId: number
+  };
+}
+
+function displayTodos ( todos ) {
+  console.log('This is Array' + todos)
+  const display = todos.map( (obj) => {
+    if (!obj.completed) {
+      return (
+        <li
+          key={`${obj.id}`}
+          className='folder-todos todoItem'
+        >
+          <span></span>
+          <span>{obj.title}</span>
+          <button onClick={() => deleteTodo(todo.id)}>🗑️</button>
+        </li>
+      );
+    }
+  })
+
+  return display
+}
+
+export default function FolderTodos({ data }: Props) {
+    const { name, todos, id} = data
+  return (
+    <>
+      {data.name}
+      <ul>
+        {displayTodos(todos)}
+      </ul>
+      
+    </>
+  )
+}
