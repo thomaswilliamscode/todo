5834450a4ec1fa3422edbf08b4469eabccddba42
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2025-12-21 01:07:12 -0500

    Remove unused useState import from TodoContext for cleaner code

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext } from 'react';
 
 import type { StateData } from '../types/state-data'
 import type { Todo } from '../types/todo'
