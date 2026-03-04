bb0299e6e769c86682df35dc5d01c8faf090a5a1
Thomas Williams <64794727+thomaswilliamscode@users.noreply.github.com>
2026-03-04 15:45:07 +0000

    vercel fix

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,20 +1,7 @@
 import SidebarFoldersAndLists from "./SidebarFoldersAndLists";
-import { Link } from "react-router-dom";
 import "../Styles/sidebar.css";
 
-import { useState } from "react";
-
 export default function Sidebar() {
-  const [isActive, setisActive] = useState(false);
-
-  function toggleActive() {
-    if (isActive) {
-      setisActive(false);
-    } else {
-      setisActive(true);
-    }
-  }
-
   return (
     <div id="sidebar">
       <SidebarFoldersAndLists />
