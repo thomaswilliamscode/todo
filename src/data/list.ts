2cc768e7025034166c5d04d4efe201b1026298c9
Thomas Williams <64794727+thomaswilliamscode@users.noreply.github.com>
2026-03-05 21:30:34 +0000

    changed data ids to strings

--- src/data/list.ts
+++ src/data/list.ts
@@ -1,7 +1,7 @@
-import type { List } from '../types/list'
-import { dummyData1, dummyData2 } from './todos'
+import type { List } from "../types/list";
+import { dummyData1, dummyData2 } from "./todos";
 
 export const listData: List[] = [
-  { type: 'list', id: 1, name: 'Today', todos: dummyData1},
-  { type: 'list', id: 2, name: 'Focus Tasks', todos: dummyData2 },
+  { type: "list", id: "1", name: "Today", todos: dummyData1 },
+  { type: "list", id: "2", name: "Focus Tasks", todos: dummyData2 },
 ];
