4337b3b027b2ed924578c6e759bec19751dd1e86
Thomas Williams <64794727+thomaswilliamscode@users.noreply.github.com>
2026-03-05 18:09:52 +0000

    header css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { useContext } from "react";
 import { useTodoContext } from "../context/TodoContext";
 import "../Styles/header.css";
 export default function Header() {
@@ -8,12 +7,18 @@ export default function Header() {
     <header className="container">
       <nav className="header">
         <ul>
-          <li className={`${focus === "home" ? "active-header" : ""}`}>
+          <li
+            className={` header-btn ${focus === "home" ? "active-header" : ""}`}
+          >
             <Link to="/" onClick={() => setFocus("home")}>
               Home
             </Link>
           </li>
-          <li className={`${focus === "focus" ? "active-header" : ""}`}>
+          <li
+            className={` header-btn ${
+              focus === "focus" ? "active-header" : ""
+            }`}
+          >
             <Link to="/dripfeed" onClick={() => setFocus("focus")}>
               Focus Mode
             </Link>
