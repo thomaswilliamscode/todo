f9cab6d0562d715f112e6255a16fc19272e880eb
Thomas Williams <64794727+thomaswilliamscode@users.noreply.github.com>
2026-03-05 22:33:11 +0000

    hidden numbers turned to string

--- src/data/todos.ts
+++ src/data/todos.ts
@@ -5,19 +5,19 @@ export const dummyData1: Todo[] = [
     id: "1",
     title: "first",
     completed: false,
-    listId: 1,
+    listId: "1",
   },
   {
     id: "2",
     title: "Build a Todo App",
     completed: true,
-    listId: 1,
+    listId: "1",
   },
   {
     id: "3",
     title: "second",
     completed: false,
-    listId: 1,
+    listId: "1",
   },
 ];
 
@@ -26,19 +26,19 @@ export const dummyData2: Todo[] = [
     id: "1",
     title: "third",
     completed: false,
-    listId: 2,
+    listId: "2",
   },
   {
     id: "2",
     title: "oh ya",
     completed: true,
-    listId: 2,
+    listId: "2",
   },
   {
     id: "3",
     title: "fourth",
     completed: false,
-    listId: 2,
+    listId: "2",
   },
 ];
 
@@ -47,19 +47,19 @@ export const dummyData3: Todo[] = [
     id: "1",
     title: "fith",
     completed: false,
-    listId: 3,
+    listId: "3",
   },
   {
     id: "2",
     title: "love on ruby",
     completed: true,
-    listId: 3,
+    listId: "3",
   },
   {
     id: "3",
     title: "sith",
     completed: false,
-    listId: 3,
+    listId: "3",
   },
 ];
 
@@ -68,19 +68,19 @@ export const dummyData4: Todo[] = [
     id: "1",
     title: "seventh",
     completed: false,
-    listId: 4,
+    listId: "4",
   },
   {
     id: "2",
     title: "love on ruby",
     completed: true,
-    listId: 4,
+    listId: "4",
   },
   {
     id: "3",
     title: "eith",
     completed: false,
-    listId: 4,
+    listId: "4",
   },
 ];
 
@@ -89,7 +89,7 @@ export const inboxData: Todo[] = [
     id: "1",
     title: "First Focus",
     completed: false,
-    listId: 0,
+    listId: "0",
   },
   {
     id: "2",
