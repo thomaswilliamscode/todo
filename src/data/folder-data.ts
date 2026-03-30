7e91d80cd50e941dbd0c4cfe45f21b0f9cbcc8c8
Thomas Williams <thomas.williams.code@gmail.com>
2026-03-30 14:44:54 -0400

    Updating folder shape

--- src/data/folder-data.ts
+++ src/data/folder-data.ts
@@ -2,6 +2,8 @@ import { v4 as uuidv4 } from "uuid";
 
 export const folder1Id: string = uuidv4();
 export const folder2Id: string = uuidv4();
+export const folder3Id: string = uuidv4();
+export const folder4Id: string = uuidv4();
 
 export const list1Id: string = uuidv4();
 export const list2Id: string = uuidv4();
@@ -15,43 +17,19 @@ const todo4: string = uuidv4();
 
 export const data = [
   {
-    type: "folder",
     id: folder1Id,
     name: "🔥 Daily Rituals",
-    open: true,
   },
   {
-    type: "folder",
     id: folder2Id,
     name: "📦 Content",
-    open: true,
+  },
+    {
+    id: folder3Id,
+    name: "🔥 Daily Rituals",
   },
   {
-    type: "list",
-    id: list1Id,
-    name: "Today!!!",
-    folderId: folder1Id,
-    todos: [{ id: todo1, title: "test", completed: false }],
-  },
-  {
-    type: "list",
-    id: list2Id,
-    name: "Focus Tasks",
-    folderId: folder1Id,
-    todos: [{ id: todo2, title: "shower", completed: false }],
-  },
-  {
-    type: "list",
-    id: list3Id,
-    name: "Video Ideas",
-    folderId: folder2Id,
-    todos: [{ id: todo3, title: "walk dogs", completed: false }],
-  },
-  {
-    type: "list",
-    id: list4Id,
-    name: "Clips to Post",
-    folderId: folder2Id,
-    todos: [{ id: todo4, title: "rant", completed: false }],
+    id: folder4Id,
+    name: "📦 Content",
   },
 ];
